a0d903f73300dfa01accd3c66add4b8456826728
alex soto <alejandro.soto@harness.io>
2026-02-17 09:55:53 -0500

    Initial commit

---
+++ src/main/java/com/harness/Springbootsample.java
@@ -0,0 +1,11 @@
+package com.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsample {
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootsample.class, args);
+    }
+}
